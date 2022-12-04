0886fc5efb36f89aade437018b6cb10e623c03ae
uberdever <uberdever@mail.ru>
2022-12-04 12:09:43 +0300

    Reworked README.md, updated requirements. Added some tests

---
+++ tests/MyBatis(Xml).Sql.sql
@@ -0,0 +1,19 @@
+<select id="selectAll" resultMap="warehouseResult">
+    SELECT * FROM crm_warehouse w
+    LEFT JOIN crm_country ON crm_country.country_id = w.country_id 
+    LEFT JOIN crm_city ON crm_city.city_id
+    WHERE TRUE = w.city_id
+    <if test="withDel == null or withDel == false">
+        AND w.warehouse_is_del = 0
+    </if>
+    <if test="warehouseIds != null and warehouseIds.size() > 0">
+        AND w.warehouse_id IN
+        <foreach item="warehouseId" collection="warehouseIds" open="(" separator="," close=")">
+            #{warehouseId}
+        </foreach>
+    </if>
+    <if test="cityId != null">
+        AND w.city_id = #{cityId}
+    </if>
+    ORDER BY w.warehouse_id
+</select>
