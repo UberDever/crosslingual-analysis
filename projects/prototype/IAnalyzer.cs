9ddd9fa87d9fc9aaf69ee5f0df01e0124805a274
uberdever <uberdever@mail.ru>
2023-01-22 18:38:33 +0300

    Added prototype and .gitignore

---
+++ projects/prototype/IAnalyzer.cs
@@ -0,0 +1,7 @@
+namespace Prototype
+{
+    interface IAnalyzer
+    {
+
+    };
+}
---
+++ projects/prototype/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace Prototype
+{
+    class Program
+    {
+        private const string HtmlPath = "tests/form.html.css.js/index.html";
+        private const string CssPath = "tests/form.html.css.js/style.css";
+        private const string JsPath = "tests/form.html.css.js/script.js";
+        static void Main()
+        {
+
+            // {
+            //     var stylesheet = new StylesheetParser().Parse(File.ReadAllText(CssPath));
+
+            //     foreach (var rule in stylesheet.StyleRules)
+            //     {
+            //         Console.WriteLine();
+            //     }
+
+            // }
+            // return;
+
+            // IConfiguration config = Configuration.Default;
+
+            // //Create a new context for evaluating webpages with the given config
+            // IBrowsingContext context = BrowsingContext.New(config);
+
+            // //Just get the DOM representation
+            // IDocument document = await context.OpenAsync(req => req.Content(File.ReadAllText(HtmlPath)));
+
+            // //Serialize it back to the console
+            // Console.WriteLine(document.DocumentElement.OuterHtml);
+            // return;
+
+            // // 
+            // var lexer = new JavascriptLexer(File.ReadAllText(JsPath));
+            // var parser = new JavascriptParser(lexer);
+            // var result = parser.Parse();
+            // if (result.IsSuccess)
+            // {
+            //     Console.WriteLine(result.Root);
+            // }
+            // else
+            // {
+            //     foreach (var err in result.Errors)
+            //     {
+            //         Console.WriteLine(err);
+            //     }
+            // }
+        }
+    }
+}
