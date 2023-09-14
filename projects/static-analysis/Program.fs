0b9b77bc829d524cf66ecb5dd9248b7c2d52cc30
Orlovsky Maxim <orlovskiy.maxim@huawei-partners.com>
2023-09-14 11:39:25 +0300

    Added static-analysis project

---
+++ projects/static-analysis/Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let upper s = 
+    s
+    |> List.map 
+
+let uppered = "Hello from F#"
+               |> String.map (System.Char.ToUpper)
+printfn "%s" uppered
