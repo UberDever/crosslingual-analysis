4bc529ecbb25b535282e2e69022895791bb9ea9e
Orlovsky Maxim <orlovskiy.maxim@huawei-partners.com>
2024-02-13 12:10:10 +0300

    Cleaned up projects

--- projects/static-analysis/Program.fs
+++
@@ -1,7 +0,0 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-
-let upper = List.map 
-
-let uppered = "Hello from F#"
-               |> String.map (System.Char.ToUpper)
-printfn "%s" uppered
